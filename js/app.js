fd3479e38555243c27390112fc7d2a04bb7cd269
ychng <>
2022-07-27 23:13:17 +0800

    換測視圖測試2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/webar.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/Test_WebGL3.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
